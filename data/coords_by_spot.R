e732f9fe5c6ac111a5c58255dcb75bdca1b80c70
leonie <leonie.platzer@gmail.com>
2025-06-23 23:45:19 +0200

    additional surf spots

--- data/coords_by_spot.R
+++ data/coords_by_spot.R
@@ -1,34 +1,67 @@
 
 spot_coords <- list(
+  # Hawaii
   "Pipeline" = c(lat = 21.664, lng = -158.051),
   "Lahaina" = c(lat = 20.878, lng = -156.682),
   "Honolua Bay" = c(lat = 21.012, lng = -156.640),
+  "Waikiki" = c(lat = 21.282, lng = -157.837),
+  "Sunset Beach" = c(lat = 21.661, lng = -158.052),
+  "Waimea Bay" = c(lat = 21.640, lng = -158.059),
   
+  # Sri Lanka
   "Weligama" = c(lat = 5.973, lng = 80.429),
   "Mirissa" = c(lat = 5.947, lng = 80.471),
   "Arugam Bay" = c(lat = 6.839, lng = 81.828),
+  "Hikkaduwa" = c(lat = 6.143, lng = 80.104),
+  "Unawatuna" = c(lat = 6.024, lng = 80.259),
+  "Midigama" = c(lat = 5.952, lng = 80.428),
   
+  # Morocco
   "Anchor Point" = c(lat = 30.531, lng = -9.708),
   "Imsouane" = c(lat = 30.838, lng = -9.815),
   "Taghazout" = c(lat = 30.545, lng = -9.711),
+  "Safi" = c(lat = 32.300, lng = -9.237),
+  "Essouira" = c(lat = 31.508, lng = -9.759),
+  "Dakhla" = c(lat = 23.684, lng = -15.957),
   
+  # Portugal
   "Nazaré" = c(lat = 39.601, lng = -9.070),
   "Ericeira" = c(lat = 38.966, lng = -9.417),
   "Peniche" = c(lat = 39.355, lng = -9.381),
+  "Supertubos" = c(lat = 39.355, lng = -9.384),
+  "Cascais" = c(lat = 38.697, lng = -9.422),
+  "Arrifana" = c(lat = 37.295, lng = -8.830),
   
+  # Indonesia (Bali area)
   "Uluwatu" = c(lat = -8.432, lng = 115.084),
   "Canggu" = c(lat = -8.647, lng = 115.138),
   "Medewi" = c(lat = -8.432, lng = 114.763),
+  "Padang Padang" = c(lat = -8.818, lng = 115.086),
+  "Kuta" = c(lat = -8.717, lng = 115.169),
+  "Keramas" = c(lat = -8.605, lng = 115.313),
   
+  # Costa Rica
   "Santa Teresa" = c(lat = 9.633, lng = -85.161),
   "Tamarindo" = c(lat = 10.299, lng = -85.841),
   "Nosara" = c(lat = 9.979, lng = -85.653),
+  "Dominical" = c(lat = 9.311, lng = -83.746),
+  "Jaco" = c(lat = 9.621, lng = -84.630),
+  "Playa Hermosa" = c(lat = 9.646, lng = -84.647),
   
+  # Australia
   "Bondi Beach" = c(lat = -33.891, lng = 151.277),
   "Snapper Rocks" = c(lat = -28.167, lng = 153.552),
   "Bells Beach" = c(lat = -38.370, lng = 144.283),
+  "Byron Bay" = c(lat = -28.647, lng = 153.613),
+  "Noosa Heads" = c(lat = -26.392, lng = 153.094),
+  "Margaret River" = c(lat = -33.954, lng = 115.073),
   
+  # France
   "Hossegor" = c(lat = 43.666, lng = -1.444),
   "Biarritz" = c(lat = 43.483, lng = -1.558),
-  "Lacanau" = c(lat = 45.002, lng = -1.197)
+  "Lacanau" = c(lat = 45.002, lng = -1.197),
+  "Seignosse" = c(lat = 43.671, lng = -1.456),
+  "Guéthary" = c(lat = 43.400, lng = -1.604),
+  "Anglet" = c(lat = 43.483, lng = -1.543)
 )
+
--- data/spots_and_countries.R
+++ data/spots_and_countries.R
@@ -1,10 +1,10 @@
 spots_by_country <- list(
-  "Hawaii" = c("Pipeline", "Lahaina", "Honolua Bay"),
-  "Sri Lanka" = c("Weligama", "Mirissa", "Arugam Bay"),
-  "Morocco" = c("Anchor Point", "Imsouane", "Taghazout"),
-  "Portugal" = c("Nazaré", "Ericeira", "Peniche"),
-  "Indonesia" = c("Uluwatu", "Canggu", "Medewi"),
-  "Costa Rica" = c("Santa Teresa", "Tamarindo", "Nosara"),
-  "Australia" = c("Bondi Beach", "Snapper Rocks", "Bells Beach"),
-  "France" = c("Hossegor", "Biarritz", "Lacanau")
+  "Hawaii" = c("Pipeline", "Lahaina", "Honolua Bay", "Waikiki", "Sunset Beach", "Waimea Bay"),
+  "Sri Lanka" = c("Weligama", "Mirissa", "Arugam Bay", "Hikkaduwa", "Unawatuna", "Midigama"),
+  "Morocco" = c("Anchor Point", "Imsouane", "Taghazout", "Safi", "Essouira", "Dakhla"),
+  "Portugal" = c("Nazaré", "Ericeira", "Peniche", "Supertubos", "Cascais", "Arrifana"),
+  "Indonesia" = c("Uluwatu", "Canggu", "Medewi", "Padang Padang", "Kuta", "Keramas"),
+  "Costa Rica" = c("Santa Teresa", "Tamarindo", "Nosara", "Dominical", "Jaco", "Playa Hermosa"),
+  "Australia" = c("Bondi Beach", "Snapper Rocks", "Bells Beach", "Byron Bay", "Noosa Heads", "Margaret River"),
+  "France" = c("Hossegor", "Biarritz", "Lacanau", "Seignosse", "Guéthary", "Anglet")
 )
